351991510a3ccbabc82b04116019a9245f3cdc97
Demid Popravko <demid1498@gmail.com>
2022-08-14 13:56:45 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
